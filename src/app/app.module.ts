adad41737e9cd284a99d1ce787802293848039ef
Bengin Cetindere <bengin@cetindere.de>
2020-12-29 15:26:16 +0100

    somewhat stable animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +19,8 @@ import { OutputColorComponent } from './output-color/output-color.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ColorSketchModule
+    ColorSketchModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/input-form/input-form.component.ts
+++ src/app/input-form/input-form.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import {Color} from 'ngx-color';
+import {Component, OnInit, HostBinding, ViewChild, ElementRef} from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
 
 @Component({
   selector: 'app-input-form',
   templateUrl: './input-form.component.html',
-  styleUrls: ['./input-form.component.sass']
+  styleUrls: ['./input-form.component.sass'],
+  animations: [
+    trigger('enlarge', [
+      state('small', style({
+        transform: 'translate(-45%, -55%) scale(0) '
+      })),
+      state('enlarged', style({
+        transform: 'translate(-50%, 25%) scale(1) '
+      })),
+      transition('small => enlarged', [
+        animate('0.3s ease')
+      ]),
+      transition('enlarged => small', [
+        animate('0.3s ease')
+      ]),
+    ]),
+  ]
 })
 export class InputFormComponent implements OnInit {
 
+  @ViewChild('preview') preview: ElementRef;
+
   color = '#aa0033';
   isOpened = false;
+
 
   constructor() { }
 
@@ -18,6 +44,12 @@ export class InputFormComponent implements OnInit {
 
   change(event: any): void {
     this.color = event.color.hex;
+  }
+
+  togglePicker(e): void{
+    this.isOpened = !this.isOpened;
+    const el = this.preview.nativeElement;
+    el.classList.toggle('is-enlarged');
   }
 
   closePicker(): void{
